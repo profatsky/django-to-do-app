12ff7d57711a763ff1594b0983d63539c31eafcc
Mikhail Profatsky <mixail.rostov04@gmail.com>
2023-01-01 20:16:08 +0300

    added task model, registered it in admin panel

--- src/tasks/admin.py
+++ src/tasks/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'user', 'is_complete', 'created_at',)
+    list_display_links = ('id', 'title')
+    list_filter = ('user', 'is_complete')
+    list_editable = ('is_complete',)
+    fields = ('title', 'user', 'is_complete', 'created_at')
+    search_fields = ('title', 'user')
