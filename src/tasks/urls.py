f7d6528569d599906e78042a550638d6de5ab212
Mikhail Profatsky <mixail.rostov04@gmail.com>
2023-01-01 20:52:13 +0300

    added views for creating, updating, deleting tasks, created base template and templates for this views

--- src/tasks/urls.py
+++ src/tasks/urls.py
@@ -1,10 +1,20 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from .views import UserRegisterView, UserLoginView
+from .views import (
+    UserRegisterView,
+    UserLoginView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView
+)
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', UserRegisterView.as_view(), name='register'),
+
+    path('task-create/', TaskCreateView.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdateView.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', TaskDeleteView.as_view(), name='task-delete'),
 ]
--- src/tasks/views.py
+++ src/tasks/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import login
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView, DeleteView
 
 from .forms import CustomUserCreationForm
+from .models import Task
 
 
 class UserLoginView(LoginView):
@@ -30,3 +32,31 @@ class UserRegisterView(CreateView):
         if self.request.user.is_authenticated:
             return redirect('task_list')
         return super().get(*args, **kwargs)
+
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    template_name = 'tasks/task_form.html'
+    model = Task
+    fields = ['title', 'description', 'is_complete']
+    success_url = reverse_lazy('task_list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    template_name = 'tasks/task_form.html'
+    model = Task
+    fields = ['title', 'description', 'is_complete']
+    success_url = reverse_lazy('task_list')
+
+
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    template_name = 'tasks/task_delete.html'
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('task_list')
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user.pk)
